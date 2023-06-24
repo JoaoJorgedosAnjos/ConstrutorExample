a8dfe26d15ffe94d79a490627b0c26253449ca73
joaojorge <joaojorgedosanjos@gmail.com>
2023-06-24 20:24:20 -0300

    Aula construtor

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class Pessoa {
+public class Pessoa {
+    private String nome;
+    private String cpf;
+    private String endereco;
+
+    public Pessoa(String nome, String cpf) {
+        this.cpf = cpf;
+        this.nome = nome;
+    }
+
+    public Pessoa() {
+    }
+
+    public Pessoa(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
 }
--- src/SistemaCadastro.java
+++ src/SistemaCadastro.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class SistemaCadastro {
+public class SistemaCadastro {
+    public static void main(String[] args) {
+        //criamos uma pessoa no sistema
+        Pessoa marcos = new Pessoa("Marcos", "04343343333");
+        Pessoa joao = new Pessoa();
+
+        //definimos o endereço de marcos
+        marcos.setEndereco("RUA DAS MARIAS");
+
+        System.out.println(marcos.getNome() + "-" + marcos.getCpf());
+    }
 }
